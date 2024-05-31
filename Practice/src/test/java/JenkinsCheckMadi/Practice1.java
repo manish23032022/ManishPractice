18ed8773aaff5fab2734ce648de8dc6c1a48dc66
manish23032022 <102226602+manish23032022@users.noreply.github.com>
2024-05-31 22:20:48 +0530

    Manish

---
+++ Practice/src/test/java/JenkinsCheckMadi/Practice1.java
@@ -0,0 +1,19 @@
+package JenkinsCheckMadi;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+    @Test
+	public void ATest() {
+		  System.out.println("Practiec first Smoke Testing");
+	}
+    @Test
+   	public void BTest() {
+   		  System.out.println("Practiec first regression Testing");
+   	}
+    
+    @Test
+   	public void CTest() {
+   		  System.out.println("Changes Files");
+   	}
+}
